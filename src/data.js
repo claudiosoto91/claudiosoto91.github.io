b71dcbcb4a81038967139fa4ebe1eaf18d974604
Claudio <claudio91soto@gmail.com>
2023-02-05 14:44:50 -0300

    Corrigiendo img

--- src/data.js
+++ src/data.js
@@ -1,32 +1,32 @@
 export const products = [
     {
         id: 1,
-        img: "https://previews.dropbox.com/p/thumb/ABz8M1r203_B1lqF8YLUg-qzX_DchOkSjF655GJ43y9PIL4eXomq0EdMxfuzdJ3_Uzj5uaJxA0q9uApxcHu9MF49ixAl4K-w7aFRsZLOJJeKIzmmLecPNbgaRsbM1rftnH-9rqvO5MEneCuHnKq_QTHpJkdkqCX6m6huF0ZWCw963-ou6uA6SYxxuxLBv9PjOvZenJExyRZDov9GyR4EPrzJzaKDpX2CsLZ8xOkbYoNj-O4BXAoW3gD-1iGuDMUI0uVgXPPOmak4-P3uiVOEjPoCwhuw2XY0XzFBHA1Q2zxNdpPIdWBIrhxUVIVFowwptclG6bLRCq0HgUEK4y4zRFf5Uq5s7mRqAHGIM1eZyRakGyYCIOiOqW12UhMW3YGxL4k/p.png",
+        img: "https://i.postimg.cc/k53ycthS/carolinaspa.png",
         link: "https://spaweb.netlify.app/"
     },
     {
         id: 2,
-        img: "https://previews.dropbox.com/p/thumb/ABwkdl-yXWjGZgCByBG0UHYkd-C3S74Ew5Pft8BS7hgYUUYOCRoNzl48wsFKAA9GFZhN6sZ6-QXF8jtU2w6U0KxsTZPNFf_NFtgUWK_QhZihDBApZ1unLdPD9qOO3f-MTm1JDXT7wfMnnamm7rWhJy37qJr4UeyQQCvFuN7WcGgnHf2jdad34l0rYW95Xtb5OqOWC96xtuT56OxZJ2KqmjrUajzBD80D6kTHEQkDDqjipFOEWDlSFFVb51zySiB2Il00amezHyxsZDlVK1GC9qbi8l76v0UgQCFtVWzLurlpT3iYos1T5wMIw2SkHrDoHHkfYN0Ews_K3XyigZ8BzcfE__txGgEUmQC8NYeDalxwYT9Kk7g8E9-SXLnLehPkRwUg_rHKE0vir5tdcJFkFzQu/p.png",
+        img: "https://i.postimg.cc/BQbNQ3Kj/guitarla.png",
         link: "https://webguitarra.netlify.app/"
     },
     {
         id: 3,
-        img: "https://previews.dropbox.com/p/thumb/ABzV-dIIJ9WQWuyxnCc1qpNb5V73o-R2I642YAEtBmUSsBGeMmWtFXlzOFbZzrXe9I1dg8AOEP1kzeQg0GkWYq0LyyAjwF7nBe0mqPhZZspHD9Q6V7pFCYKroMh5dBFmjpWBaIN2sXO4YP9bPQAb-UflwA44lz233pg6liRBDpNY4sEzDwb2aTeu4gDIAcuqVluPvhgmkITbHaK9_prSt5TgxCVgQzQzZ9TlCAr1ws0z_ND5AOx4eFmFpGZ_DWh9viZAEToVls1PddotDJ2aSr1DnhUHBn3ZuEAxwnGOlOPOGhUZ6hD83Gjg_HIsDOdKf1KZZj4kIztQgeeX4j2wrDc_GztXL51AIfvg1axaS4YJLqXlfjY6wAJHMBlWKVTJL7pgpJS0ijJi3IeLvyTdiyes/p.png",
+        img: "https://i.postimg.cc/kXHs2HQf/inmobiliaria.png",
         link: "https://inmobiliaria2023.netlify.app/"
     },
     {
         id: 4,
-        img: "https://previews.dropbox.com/p/thumb/ABwOHopdqUh6SeAlOfCjB5SM0bwQFbNHudsDzS1oSSUcjGhZTStL51mMSLmFKE9bZvNsfSsoSLkuAiMB8s8yK9Jt9KOAp4bfAtwG0tRh6Gg6fa__vB1GqOWCdDpptdFc2iDf1H0pcEHKy7Sx_Z6PQ8VAHi0NwHiEHKUr99t5jZnlJt8l1bNmhC67O2H0lCEr_sKbpEma_HqQkSyst5lciPOEPG-slpTBtkgfy_5B0WyvqeuXD1clVaykxQBf4FRZ_CEL8ZZ-GCU22C5gFmrx732saUesPGdO80Lyee6GHBYDxwrouWJABON5hMudYjXI70L-S1z98zJ9hLWXBqt8P4XZh9PhAAVSSeLkTjsL0GKHak_l0d6FR29o3xBt3jSmteY/p.png",
+        img: "https://i.postimg.cc/d1Gp6YBs/petshop.png",
         link: "https://petshoptuc.vercel.app/"
     },
     {
         id: 5,
-        img: "https://previews.dropbox.com/p/thumb/ABymNhzj_tC6G5uRbxPmfQv6sK_y82bscbrUw6YJOjkNElqTZi6zJI4oO0aSuxyGILWd-Cs_Dz8CeiIybHSRqhuUcBRd8h7zf_ifcCs9b56Ym3w0hrf-l3vwguuYZg9uUnPteafUR6VHmF-NZbdww_fbShShY1Hd9CcvYJ9F6t2iagD47YTegtuux-P58hIj5CF3TiJWBSBrcDRC9hcpHLxDmhA-7HcMBNrxOJs3TR8XY0PYhU_ikEVAhN1JgQUqmdmejQlOGCpJsE9eB-6uphIsugS7f66NwAW87qrmPsbGI0vEp9yKdTBmvu1HT-zSQeNhZx6qI-6AYMabS3cEYdKkZ7WDwDpHtthbgiWnyKLgbNyofawtzfKkp6NIpnuTKxhyw2lK54BN8UgjnXO_BL2f/p.png",
+        img: "https://i.postimg.cc/sXPHSJj1/pedibar.png",
         link: "https://pedi-bar.vercel.app/"
     },
     {
         id: 6,
-        img: "https://previews.dropbox.com/p/thumb/ABwVXi5Wzpom4WYfy7SOFADPIHLol8omKmsp2mSg5c0UeOpO3vhz_ErnVUW7Ib2Dlezk0fnor4LexzMcLG_E7zvOZ0QjCQlFZuBwcQd6T1NPUJcvPL2TsCOkE95fFvV3Xf3mDlI-ZqfNDb1VdqCs0LAqgykjiSa5MxPUR-N8lHtq8ZLNrBM6c1IfoRjKSUyECOgwdA7KU9sKbeO2Ragd3ehgWJnYC_7hInDnn5HfeiiZ38uyM6OB6uylUG-QSIa4AjOm6n3ejyESZYLIb4Qp45mkb8ZsxjMEJbJ4oEqWYJ2Z_qOLNp82LNQXQzXIo5iEZWXo0flixYznpPpcu-kNleiuzJa-7ACUgEWjRYRSGS22MOOi6ZQPMkP5v2jxcy7-IDRNirdBns9ZScu-eZk0x9f0/p.png",
+        img: "https://i.postimg.cc/wvkF3T2m/vivero.png",
         link: "https://vivero-75698.web.app/"
     }
 ];
